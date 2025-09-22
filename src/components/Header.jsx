6cbc63ca2152880f52316c03e0d6e7d063771b05
Alan Chan <alanchanzepeda@gmail.com>
2025-09-22 00:25:46 -0600

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,12 @@
 import { Link } from "react-router-dom";
-
+import ScorePill from "./ScorePill.jsx";
 
 export default function Header(){
   return (
     <header className="header">
       <div className="container row space-between center">
-        <Link to="/" className="brand">Country Quiz</Link>
-        <div className="score-pill" aria-live="polite">🏆 0 / 10</div>
+        <Link to="/quiz" className="brand">Country Quiz</Link>
+        <ScorePill />
       </div>
     </header>
   );
